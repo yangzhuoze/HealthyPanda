a1b6bdb8bec2573786ab412e09af219208921358
YangZhuoze <yangzhuoze@163.com>
2018-11-18 14:32:52 +0800

    add social media scanner

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.get('/users/:id', function (req, res) {
 app.post('/users', function (req, res) {
     var type = req.params.type
     var source = req.params.source
+
+
     if (type == 'bank') {
         if (source == 'HSBC') {
             // TODO Retrieve user info from HSBC
@@ -34,18 +36,51 @@ app.post('/users', function (req, res) {
             // TODO Retrieve user info from China Construction Bank
         }
     } else if (type == 'SocialMedia') {
-        if (source == 'Wechat') {
-            // TODO Retrieve user info from wechat
-        } else if (source == 'Facebook') {
-            // TODO Retrieve user info from Facebook
-        } else if (source == 'Alibaba') {
-            // TODO Retrieve user info from Alibaba
-        } else if (source == 'Linkedin') {
-            // TODO Retrieve user info from Linkedin
-        }
+        scanUserSocialMedia()
     }
     // ... more
     // TODO persist into DB.
 })
+
+var scanUserSocialMedia = function (req, res) {
+    const request = require('request');
+    const fs = require('fs');
+
+    // Access user public profile usign agent's authenticated token
+    var token = req.params.token
+    var userId = req.params.userId
+    var dir = ''
+    if (source == 'Wechat') {
+        // Scan wechat user's circle picture
+        // get post list
+        var posts = []
+        request(`http://api.wechat.com/users/${userId}/circles`, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                posts.push(body)
+                dir = '/app/wechat/users/' + userId;
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir);
+                }
+            }
+        });
+
+        if (posts !== undefined || posts.length === 0) {
+            for (post in posts) {
+                request(`http://api.wechat.com/users/${userId}/circles/${post.id}`, function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        fs.writeFile(`${dir}/${post.id}`, message);
+                    }
+                });
+            }
+        }
+    } else if (source == 'Facebook') {
+        // TODO Retrieve user info from Facebook
+        // ... same as above, but to change the API url
+    } else if (source == 'Alibaba') {
+        // TODO Retrieve user info from Alibaba
+    } else if (source == 'Linkedin') {
+        // TODO Retrieve user info from Linkedin
+    }
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
