e210e113dbc951b2eee8508f6170f893053f1e7f
YangZhuoze <yangzhuoze@163.com>
2018-11-18 13:08:07 +0800

    add api for user info collection

--- index.js
+++ index.js
@@ -22,4 +22,30 @@ app.get('/users/:id', function (req, res) {
     }
 })
 
+app.post('/users', function (req, res) {
+    var type = req.params.type
+    var source = req.params.source
+    if (type == 'bank') {
+        if (source == 'HSBC') {
+            // TODO Retrieve user info from HSBC
+        } else if (source == 'SC') {
+            // TODO Retrieve user info from Standard Chartered
+        } else if (source == 'CCB') {
+            // TODO Retrieve user info from China Construction Bank
+        }
+    } else if (type == 'SocialMedia') {
+        if (source == 'Wechat') {
+            // TODO Retrieve user info from wechat
+        } else if (source == 'Facebook') {
+            // TODO Retrieve user info from Facebook
+        } else if (source == 'Alibaba') {
+            // TODO Retrieve user info from Alibaba
+        } else if (source == 'Linkedin') {
+            // TODO Retrieve user info from Linkedin
+        }
+    }
+    // ... more
+    // TODO persist into DB.
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
